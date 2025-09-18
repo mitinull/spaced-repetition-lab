575c8f6801503219fafed8c662516bb661c4dabd
mitinull <mitinull@gmail.com>
2025-09-18 16:07:47 +0330

    implement leitner

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,108 +2,149 @@ import {
   Button,
   Divider,
   Stack,
-  ToggleButton,
-  ToggleButtonGroup,
+  Select,
+  MenuItem,
+  InputLabel,
+  FormControl,
 } from "@mui/material";
-import { Fragment, useState } from "react";
+import { Fragment, useCallback, useState } from "react";
 import { CardsForToday } from "./CardsForToday";
 import { Column } from "./Column";
 import { CardSM2 } from "./CardSM2";
+import { CardLeitner } from "./CardLeitner";
 
 export function Board() {
-  const [algorithm, setAlgorithm] = useState("Leitner");
+  const [algorithm, setAlgorithm] = useState("leitner");
   const [cards, setCards] = useState([
     {
       id: 1,
       value: "apple",
-      currentDay: 0,
-      ease: 2.5,
-      repetition: 0,
-      lastInterval: 0,
+      reviewStatus: {
+        leitner: {
+          box: 0,
+          reviewDay: 0,
+          lastInterval: 0,
+        },
+        sm2: {
+          ease: 2.5,
+          reviewDay: 0,
+          repetition: 0,
+          lastInterval: 0,
+        },
+      },
     },
     {
       id: 2,
       value: "banana",
-      currentDay: 2,
-      ease: 2.5,
-      repetition: 0,
-      lastInterval: 0,
+      reviewStatus: {
+        leitner: {
+          box: 0,
+          reviewDay: 0,
+          lastInterval: 0,
+        },
+        sm2: {
+          ease: 2.5,
+          reviewDay: 0,
+          repetition: 0,
+          lastInterval: 0,
+        },
+      },
     },
     {
       id: 3,
       value: "carrot",
-      currentDay: 2,
-      ease: 2.5,
-      repetition: 0,
-      lastInterval: 0,
+      reviewStatus: {
+        leitner: {
+          box: 0,
+          reviewDay: 0,
+          lastInterval: 0,
+        },
+        sm2: {
+          ease: 2.5,
+          reviewDay: 0,
+          repetition: 0,
+          lastInterval: 0,
+        },
+      },
     },
   ]);
 
-  function updateCardSM2({ cardId, newInterval, newEase, newRepetition }) {
-    setCards(
-      cards.map((card) =>
-        card.id === cardId
-          ? {
-              ...card,
-              ease: newEase,
-              currentDay: newInterval,
-              lastInterval: newInterval,
-              repetition: newRepetition,
-            }
-          : card
-      )
-    );
-  }
-
-  const columns = cards.reduce((acc, card) => {
-    while (card.currentDay >= acc.length) {
-      acc.push([]);
-    }
-    acc[card.currentDay].push(card);
-    return acc;
-  }, []);
+  const updateCard = useCallback(
+    ({ id, reviewStatus }) =>
+      setCards((prevCards) =>
+        prevCards.map((card) =>
+          card.id === id ? { ...card, reviewStatus } : card
+        )
+      ),
+    []
+  );
 
   function goNextDay() {
     setCards(
       cards.map((card) => ({
         ...card,
-        currentDay: Math.max(0, card.currentDay - 1),
+        reviewStatus: {
+          ...card.reviewStatus,
+          [algorithm]: {
+            ...card.reviewStatus[algorithm],
+            reviewDay: Math.max(0, card.reviewStatus[algorithm].reviewDay - 1),
+          },
+        },
       }))
     );
   }
 
+  const columns = cards.reduce((acc, card) => {
+    while (card.reviewStatus[algorithm].reviewDay >= acc.length) {
+      acc.push([]);
+    }
+    acc[card.reviewStatus[algorithm].reviewDay].push(card);
+    return acc;
+  }, []);
+
+  const leitner = useCallback(
+    (props) => <CardLeitner {...props} onCardMove={updateCard} />,
+    [updateCard]
+  );
+
+  const sm2 = useCallback(
+    (props) => <CardSM2 {...props} onCardMove={updateCard} />,
+    [updateCard]
+  );
+
+  const cardComponent = algorithm === "leitner" ? leitner : sm2;
+
   return (
-    <Stack gap={1}>
+    <Stack px={1.5} py={2} gap={1}>
       <Stack alignItems="start" direction="row" gap={1}>
         <Button variant="contained" onClick={goNextDay}>
           Next Day
         </Button>
-        <ToggleButtonGroup
-          exclusive
-          size="small"
-          color="primary"
-          variant="contained"
-          value={algorithm}
-          onChange={(_, value) => setAlgorithm(value)}
-        >
-          <ToggleButton value="Leitner">Leitner</ToggleButton>
-          <ToggleButton value="SM-2">SM-2</ToggleButton>
-        </ToggleButtonGroup>
+        <FormControl size="small">
+          <InputLabel>Algorithm</InputLabel>
+          <Select
+            label="Algorithm"
+            value={algorithm}
+            onChange={(event) => setAlgorithm(event.target.value)}
+          >
+            <MenuItem value="leitner">Leitner</MenuItem>
+            <MenuItem value="sm2">SM-2</MenuItem>
+          </Select>
+        </FormControl>
       </Stack>
       <Divider />
       <Stack direction={"row"}>
-        <CardsForToday
-          cards={columns.at(0)}
-          cardComponent={(props) => (
-            <CardSM2 {...props} onCardMove={updateCardSM2} />
-          )}
-        />
+        <CardsForToday cards={columns.at(0)} cardComponent={cardComponent} />
       </Stack>
       <Divider />
       <Stack direction="row" gap={2} flexWrap="wrap">
         {columns.slice(1).map((column, index) => (
           <Fragment key={index}>
-            <Column index={index} column={column} />
+            <Column
+              index={index}
+              column={column}
+              cardComponent={cardComponent}
+            />
             <Divider orientation="vertical" flexItem />
           </Fragment>
         ))}
@@ -111,3 +152,11 @@ export function Board() {
     </Stack>
   );
 }
+
+/* TODO:
+- Add data generator
+- Add local storage
+- Add button "Add Card"
+- Add "Add X card everyday" input
+- Add inputs for "Move X days forward with random weighted selection (.9 correct or .5 correct etc.)"
+*/
